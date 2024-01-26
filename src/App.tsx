3ec8bff0c8a7c5a4faf1bcdbee4a3b7a0ce5d1dc
Seefa Banu <114132712+seefaBanu@users.noreply.github.com>
2024-01-26 09:17:56 +0530

    user related simple content change

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,29 @@ import './App.css'
 
 function App() {
 
+  const [user, setUser] = useState('user')
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  if(isAuthenticated && user === 'admin') {
+    return (
+      <>
+        <h1>Welcome {user}</h1>
+        <button onClick={()=> setIsAuthenticated(false)}>Sign out</button>
+      </>
+    )
+  }
+  if(isAuthenticated && user === 'user') {
+    return (
+      <>
+        <h1>Welcome {user}</h1>
+        <button onClick={()=> setIsAuthenticated(false)}>Sign out</button>
+      </>
+    )
+  }
+
   return (
     <>
-       <button>Sign in</button>
+       <button onClick={()=> setIsAuthenticated(true)}>Sign in</button>
     </>
   )
 }
