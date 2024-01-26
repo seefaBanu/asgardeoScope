3582351c062c56820fc83f09da4d15f696862f55
Seefa Banu <114132712+seefaBanu@users.noreply.github.com>
2024-01-26 10:15:46 +0530

    asgardeo sdk add

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import { useAuthContext } from '@asgardeo/auth-react'
 
 function App() {
 
-  const [user, setUser] = useState('')
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-
-
-
-  const handelSignOut = () => {
-    setIsAuthenticated(false);
-    setUser('');
-  }
-
-  const handelSignIn = () => {
-    setIsAuthenticated(true);
-  }
+  const {state,signIn,signOut} = useAuthContext();
 
   return (
     <>
-      {isAuthenticated ? (
+      {state.isAuthenticated ? (
         <>
-          <h1>Welcome {user}</h1>
-          <button onClick={() => handelSignOut()}>Sign out</button>
+          {/* <h1>Welcome {stateuser}</h1> */}
+          <button onClick={() => signOut}>Sign out</button>
         </>
       ) :
         (
           <>
-            <div style={{ margin: '20px' }}>
-              <label style={{ marginRight: '20px' }}>Username</label>
-              <input type="text" onChange={(e) => setUser(e.target.value)} style={{ marginBottom: '10px' }} />
-            </div>
-            <button onClick={() => handelSignIn()}>Sign in</button>
-
+            <button onClick={() => signIn()}>Sign in</button>
           </>
         )}
 
